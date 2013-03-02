4ed8c325957c44a493f4ec99815aa16d553e3e8d
Christian Kolb <christian.kolb@coseven.com>
2013-03-02 16:48:32 +0100

    Add copyright comment back
    Add copyright comment back which was removed in the last commit

--- src/system/modules/metamodelsattribute_alias/languages/de/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_alias/languages/de/tl_metamodel_attribute.php
@@ -1,5 +1,19 @@
 <?php
-
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   MetaModels
+ * @subpackage Backend
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Christian Kolb <info@kolbchristian.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
 if (!defined('TL_ROOT'))
 {
 	die('You cannot access this file directly!');
