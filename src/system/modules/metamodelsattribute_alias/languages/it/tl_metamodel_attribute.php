f8168a835ccebec79e1abfa4d89e639b3d059c9b
Mini Model <minimodel@metamodel.me>
2013-06-23 04:12:31 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_alias/languages/it/tl_metamodel_attribute.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/it/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-04T02:35:06+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['0']    = 'Campi alias';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['1']    = 'Seleziona uno o più attributi per combinare un alias.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']      = 'Attributi';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias']['0']     = '​​Forza rigenerazione alias';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias']['1']     = 'Seleziona per rigenerare l\'alias ogni volta che uno dei campi dipendenti si cambia. Nota che questa operazione fa invalido gli URL già esistenti che dipendono dall\'alias.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['alias'] = 'Alias';
