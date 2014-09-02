17c26269b2cfd908c102e77ac2c686be43b721f5
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-09-02 04:59:20 +0200

    Reindent dca file.

--- src/system/modules/metamodelsattribute_alias/dca/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_alias/dca/tl_metamodel_attribute.php
@@ -21,42 +21,41 @@
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['alias extends _simpleattribute_'] = array
 (
-	'+advanced' => array('force_alias'),
-	'+display'  => array('alias_fields after description')
+    '+advanced' => array('force_alias'),
+    '+display'  => array('alias_fields after description')
 );
-
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['alias_fields'] = array
 (
-	'label'                 => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields'],
-	'exclude'                 => true,
-	'inputType'               => 'multiColumnWizard',
-	'eval'                    => array
-	(
-		'columnFields' => array
-		(
-			'field_attribute' => array
-			(
-				'label'                 => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute'],
-				'exclude'               => true,
-				'inputType'             => 'select',
-				'eval'                  => array
-					(
-					'style'             => 'width:600px',
-					'chosen'            => 'true'
-					)
-			),
-		),
-	),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields'],
+    'exclude'   => true,
+    'inputType' => 'multiColumnWizard',
+    'eval'      => array
+    (
+        'columnFields' => array
+        (
+            'field_attribute' => array
+            (
+                'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute'],
+                'exclude'   => true,
+                'inputType' => 'select',
+                'eval'      => array
+                (
+                    'style'  => 'width:600px',
+                    'chosen' => 'true'
+                )
+            ),
+        ),
+    ),
 );
 
 $GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['force_alias'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array
-	(
-		'tl_class'            => 'cbx w50'
-	),
+    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias'],
+    'exclude'   => true,
+    'inputType' => 'checkbox',
+    'eval'      => array
+    (
+        'tl_class' => 'cbx w50'
+    ),
 );
