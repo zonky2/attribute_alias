014f5a2530ce5019203cd5bb3c57561068049b61
Mini Model <minimodel@metamodel.me>
2015-09-18 09:47:22 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_attribute.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-05-20T12:13:36+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['0']    = 'Псевдоним поля';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['1']    = 'Выберите один или несколько атрибутов для объединения алиаса.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']      = 'Атрибуты';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias']['0']     = 'Принудительно регенерировать алиас';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias']['1']     = 'Выберите, для регенерации алиаса каждый раз при изменении любого из подчиненных полей. Обратите внимание, что это лишит вас старых URL-адресов основанных на алиасе.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['alias'] = 'Алиас';
+
